1d0dee9c947bc1f193476c63694e34b1ed9dee5d
Arlana(Zhanbota) Dyussebayeva <arlanakznl@gmail.com>
2021-04-24 01:05:56 +0600

    Initial commit(React router is on) work on subtotal

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDB21sDmI2iUdfCXOyztbSKmuACjT5pC3w",
+    authDomain: "clone-1b94d.firebaseapp.com",
+    databaseURL: "https://clone-1b94d.firebaseio.com",
+    projectId: "clone-1b94d",
+    storageBucket: "clone-1b94d.appspot.com",
+    messagingSenderId: "813361941350",
+    appId: "1:813361941350:web:57ee30405c0b514384d6a7",
+    measurementId: "G-6CDBQVMFRV"
+  };
